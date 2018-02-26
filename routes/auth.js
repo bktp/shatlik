c355ee6db798c582fca51d878ba96af4315e9ffc
bktp <os.buktp@gmail.com>
2018-02-26 09:41:59 +0300

    damn

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const auth = require('../auth')
 router.post('/', (req,res) => {
     if (auth.isAuth(req.body.password)) {
         res.cookie('password', req.body.password, {maxAge: 10000000})
+        res.send()
     } else {
         res.json({error: "Something went wrong"})
     }
