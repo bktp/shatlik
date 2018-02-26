83192943f32e657c5453833cc8d38d0959105b71
bktp <os.buktp@gmail.com>
2018-02-26 08:01:57 +0300

    crypto

--- auth.js
+++ auth.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto')
+
 function isAuth(password) {
     (crypto.createHash('sha256').update(password).digest('base64') == 'wSvJgmeUZCyX1WrVSIykOjbuBp3rf72B/eJt32LyLSQ=') ? true : false
 }
