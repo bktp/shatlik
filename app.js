1337c5ec939672b463889ebb0ee6b6005b6864f4
bktp <os.buktp@gmail.com>
2018-02-26 09:02:42 +0300

    1

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://shatlik.ru');
+  res.header('Access-Control-Allow-Origin', 'http://shatlik.ru, shatlik.ru');
   // res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
--- routes/servicesSpots.js
+++ routes/servicesSpots.js
@@ -7,33 +7,38 @@ const auth = require('../auth')
 
 router.get('/', (req, res) => {
     db.one('SELECT * FROM spots')
-    .then((data) => {
-        res.send(JSON.stringify(data))
-    })
-    .catch((err) => {
-        res.send(JSON.stringify({error: err.message}))
-    })
+        .then((data) => {
+            res.send(JSON.stringify(data))
+        })
+        .catch((err) => {
+            res.send(JSON.stringify({
+                error: err.message
+            }))
+        })
 });
 
 router.use((req, res, next) => {
     console.log("Проверка пароля началась")
-    console.dir(req.cookies)
-    if (auth.isAuth(req.cookies.password)) 
-    {
+    console.dir(req)
+    if (auth.isAuth(req.cookies.password)) {
         console.log("Проверка пройдена")
         next()
+    } else {
+        res.send()
     }
 })
 
 router.put('/', (req, res) => {
     console.log(req.body)
     db.query("UPDATE spots SET business=${business}, stac=${stac}, semistac=${semistac}, updated=TO_DATE(${updated}, 'DD.MM.YYYY')", req.body)
-    .then(() => {
-        res.send("Success")
-    })
-    .catch((err) => {
-        res.send(JSON.stringify({error:err.message}))
-    })
+        .then(() => {
+            res.send("Success")
+        })
+        .catch((err) => {
+            res.send(JSON.stringify({
+                error: err.message
+            }))
+        })
 });
 
 module.exports = router;
