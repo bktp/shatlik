9624bb0c91692c27226d2a241e70e0884625b850
osbuktp <os.buktp@yandex.ru>
2021-01-21 15:40:42 +0300

    updated express

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,24 @@
-const express = require('express')
-const router = express.Router()
-const auth = require('../auth')
+const express = require("express");
+const router = express.Router();
+const auth = require("../auth");
 
-router.get('/', (req,res) => {
-    if (req.cookies.password) res.json({auth: auth.isAuth(req.cookies.password)})
-    else res.json({auth: false})
-})
+router.get("/", (req, res) => {
+  if (req.cookies.password)
+    res.json({ auth: auth.isAuth(req.cookies.password) });
+  else res.json({ auth: false });
+});
 
-router.post('/', (req,res) => {
-    if (auth.isAuth(req.body.password)) {
-        res.cookie('password', req.body.password, {maxAge: 10000000, sameSite: 'none'})
-        res.send()
-    } else {
-        res.json({error: "Something went wrong"})
-    }
-})
+router.post("/", (req, res) => {
+  if (auth.isAuth(req.body.password)) {
+    res.cookie("password", req.body.password, {
+      maxAge: 10000000,
+      sameSite: "none",
+      secure: true,
+    });
+    res.send();
+  } else {
+    res.json({ error: "Something went wrong" });
+  }
+});
 
-module.exports = router
+module.exports = router;
